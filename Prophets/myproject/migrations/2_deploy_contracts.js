2a9fde7d7e8c765177d5c1cbb704194b4555013e
Stan <iamjacke@gmail.com>
2018-01-06 07:12:24 +0300

    front end improvements

--- Prophets/myproject/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
-var ExampleToken = artifacts.require("./ExampleToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ProofOfExistence1);
-  deployer.deploy(ExampleToken);
-};
